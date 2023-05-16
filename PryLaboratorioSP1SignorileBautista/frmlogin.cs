13471eb5f5a7b9060f2a632286c90e0c2fdc8fbf
BAUTISTASIGNORILE@ies21.onmicrosoft.com <BAUTISTASIGNORILE@ies21.onmicrosoft.com>
2023-05-15 23:05:33 -0300

    15/5/2023

--- PryLaboratorioSP1SignorileBautista/frmlogin.cs
+++ PryLaboratorioSP1SignorileBautista/frmlogin.cs
@@ -30,7 +30,7 @@ namespace PryLaboratorioSP1SignorileBautista
 
         private void cmdAceptar_Click(object sender, EventArgs e)
         {
-            if (txtUsuario.Text == "Adm" && txtContraseña.Text == "@1a" && cmbModulo.Text == "ADM")
+            if (txtUsuario.Text == "Adm" && txtContraseña.Text == "@1a" && (cmbModulo.Text == "ADM" || cmbModulo.Text == "COM" || cmbModulo.Text == "VTA") || txtUsuario.Text == "John" && txtContraseña.Text == "*2b" && cmbModulo.Text == "SIST" || txtUsuario.Text == "Ceci" && txtContraseña.Text == "*@3c" && (cmbModulo.Text == "ADM" || cmbModulo.Text == "VTA") || txtUsuario.Text == "God" && txtContraseña.Text == "*@#4d" && (cmbModulo.Text == "ADM" || cmbModulo.Text == "SIST" || cmbModulo.Text == "COM" || cmbModulo.Text == "VTA"))
             {
                 Form Bienvenido = new frmBienvenida();
                 this.Hide();
@@ -43,6 +43,11 @@ namespace PryLaboratorioSP1SignorileBautista
                 txtUsuario.Clear();
                 txtContraseña.Clear();
             }
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
